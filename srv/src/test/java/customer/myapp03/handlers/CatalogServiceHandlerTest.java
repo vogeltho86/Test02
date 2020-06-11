809700e641219f2c8bd28954212bb4b3013b1648
Thomas <vogeltho@arcor.de>
2020-06-11 21:17:25 +0200

    ff
    df

---
+++ srv/src/test/java/customer/myapp03/handlers/CatalogServiceHandlerTest.java
@@ -0,0 +1,43 @@
+package customer.myapp03.handlers;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.stream.Stream;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import cds.gen.catalogservice.Books;
+import customer.myapp03.handlers.CatalogServiceHandler;
+
+public class CatalogServiceHandlerTest {
+
+	private CatalogServiceHandler handler = new CatalogServiceHandler();
+	private Books book = Books.create();
+
+	@Before
+	public void prepareBook() {
+		book.setTitle("title");
+	}
+
+	@Test
+	public void testDiscount() {
+		book.setStock(500);
+		handler.discountBooks(Stream.of(book));
+		assertEquals("title (discounted)", book.getTitle());
+	}
+
+	@Test
+	public void testNoDiscount() {
+		book.setStock(100);
+		handler.discountBooks(Stream.of(book));
+		assertEquals("title", book.getTitle());
+	}
+
+	@Test
+	public void testNoStockAvailable() {
+		handler.discountBooks(Stream.of(book));
+		assertEquals("title", book.getTitle());
+	}
+
+}
